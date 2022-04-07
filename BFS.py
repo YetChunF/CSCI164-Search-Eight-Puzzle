bd02f5313707ddd9bb4494e21be7507bf59a11ad
Fong YC <73017262+YetChunF@users.noreply.github.com>
2022-04-07 13:08:38 -0700

    BFS updated

--- BFS.py
+++ BFS.py
@@ -1,21 +1,23 @@
-from queue import Queue
-from puzzle import Puzzle
+from lib2to3.pytree import Node
+from puzzle import puzzle
+from Node import node
+import queue 
 
+def breath_first_search(initial_state):
+    node = Node(puzzle.initial[:], None, "",0, puzzle.initial.index("0"))
+    frontier = queue.Queue(0)
+    frontier.put(node)
+    reached = {''.join(node.state) : True}
+    nodesExpanded = 0 
+    while not frontier.empty(): 
+        node = frontier.get()
+        nodesExpanded += 1 
+        for child in puzzle.expandNode(node):
+            hState = ''.join(child.state)
+            if puzzle.isGoal(child.state): 
+                return (child, nodesExpanded)
+            if not reached.get(hState):
+                reached[hState] = True 
+                frontier.put(child)
+    return(Node("Failure", None, None, None, None), nodesExpanded)
 
-def breadth_first_search(initial_state):
-    start_node = Puzzle(initial_state, None, None, 0)
-    if start_node.goal_test():
-        return start_node.find_solution()
-    q = Queue()
-    q.put(start_node)
-    explored=[]
-    while not(q.empty()):
-        node=q.get()
-        explored.append(node.state)
-        children=node.generate_child()
-        for child in children:
-            if child.state not in explored:
-                if child.goal_test():
-                    return child.find_solution()
-                q.put(child)
-    return
