5e76318c87f6f7b19b929ce1536e8b388ddb87ee
Yii Choong <yiichoongk@gmail.com>
2022-04-11 20:45:52 -0700

    dfs utils

--- IDDFS.py
+++ IDDFS.py
@@ -2,7 +2,7 @@ from Puzzle import PuzzleState
 from Actions import subNodes
 
 
-def depth_first_search_utils(state, goalState):
+def depth_first_search_utils(state, goalState, depth):
 
     global GoalNode, numberOfNodes
 
@@ -16,21 +16,26 @@ def depth_first_search_utils(state, goalState):
         if node.state == goalState:
             GoalNode = node
             return stack
-        #inverse the order of next paths for execution porpuses
-        possiblePaths = reversed(subNodes(node))
-        numberOfNodes += 1
-        for path in possiblePaths:
-            if path.map not in visited:
-                stack.append(path)
-                visited.add(path.map)
+        elif depth < 0:
+            continue
+        else:
+            #inverse the order of next paths for execution porpuses
+            possiblePaths = reversed(subNodes(node))
+            numberOfNodes += 1
+            for path in possiblePaths:
+                if path.map not in visited:
+                    stack.append(path)
+                    visited.add(path.map)
+        depth -= 1
+
 
 def iterative_deepening_depth_first_search(state, goal, depth):
 
-    # global GoalNode, numberOfNodes
+    global GoalNode, numberOfNodes, node
 
     # Repeatedly depth-limit search till the maximum depth
     for i in range(depth):
-        result = depth_first_search_utils(state, goal)
+        result = depth_first_search_utils(state, goal, i)
         if result:
-            return result
+            return (result, GoalNode)
     return []
