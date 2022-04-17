ad5be9ddb974528d963834d0425db1104cc332d9
Yii Choong <yiichoongk@gmail.com>
2022-04-17 15:44:41 -0700

    trying out 15 puzzles

--- Main.py
+++ Main.py
@@ -7,7 +7,9 @@ from Puzzle import PuzzleState
 
 def main():
      
-    GoalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    # GoalState = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    GoalState = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    GoalStateLong = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     
     #Obtain information from calling parameters
     parser = argparse.ArgumentParser()
@@ -17,91 +19,143 @@ def main():
     data = args.initialBoard.split(",")
 
     #Build initial board state
-    InitialState = [] #Take in 8 data 
-    InitialState.append(int(data[0]))
-    InitialState.append(int(data[1]))
-    InitialState.append(int(data[2]))
-    InitialState.append(int(data[3]))
-    InitialState.append(int(data[4]))
-    InitialState.append(int(data[5]))
-    InitialState.append(int(data[6]))
-    InitialState.append(int(data[7]))
-    InitialState.append(int(data[8]))
+    # InitialState = [] #Take in 8 data 
+    # InitialState.append(int(data[0]))
+    # InitialState.append(int(data[1]))
+    # InitialState.append(int(data[2]))
+    # InitialState.append(int(data[3]))
+    # InitialState.append(int(data[4]))
+    # InitialState.append(int(data[5]))
+    # InitialState.append(int(data[6]))
+    # InitialState.append(int(data[7]))
+    # InitialState.append(int(data[8]))
+
+    # InitialState = [] #Take in 8 data 
+    # InitialState.append(str(data[0]))
+    # InitialState.append(str(data[1]))
+    # InitialState.append(str(data[2]))
+    # InitialState.append(str(data[3]))
+    # InitialState.append(str(data[4]))
+    # InitialState.append(str(data[5]))
+    # InitialState.append(str(data[6]))
+    # InitialState.append(str(data[7]))
+    # InitialState.append(str(data[8]))
+
+    InitialStateLong = [] #Take in 8 data 
+    InitialStateLong.append(str(data[0]))
+    InitialStateLong.append(str(data[1]))
+    InitialStateLong.append(str(data[2]))
+    InitialStateLong.append(str(data[3]))
+    InitialStateLong.append(str(data[4]))
+    InitialStateLong.append(str(data[5]))
+    InitialStateLong.append(str(data[6]))
+    InitialStateLong.append(str(data[7]))
+    InitialStateLong.append(str(data[8]))
+    InitialStateLong.append(str(data[9]))
+    InitialStateLong.append(str(data[10]))
+    InitialStateLong.append(str(data[11]))
+    InitialStateLong.append(str(data[12]))
+    InitialStateLong.append(str(data[13]))
+    InitialStateLong.append(str(data[14]))
+    InitialStateLong.append(str(data[15]))
 
     function = args.method
 
-    if(function == "bfs"):
-        BFS.breath_first_search(InitialState, GoalState)
+    # if(function == "bfs"):
+    #     BFS.breath_first_search(InitialState, GoalState)
 
-        bfsNode = BFS.GoalNode
-        bfsMoves = []
+    #     bfsNode = BFS.GoalNode
+    #     bfsMoves = []
 
-        while InitialState != bfsNode.state:
-            if bfsNode.move == 1:
+    #     while InitialState != bfsNode.state:
+    #         if bfsNode.move == 1:
+    #             path = 'U'
+    #         if bfsNode.move == 2:
+    #             path = 'D'
+    #         if bfsNode.move == 3:
+    #             path = 'L'
+    #         if bfsNode.move == 4:
+    #             path = 'R'
+    #         bfsMoves.insert(0, path)
+    #         bfsNode = bfsNode.parent
+
+    #     #Print results
+    #     print("Path: ", bfsMoves)
+    #     print("Cost: ", len(bfsMoves))
+    #     print("Nodes expanded: ", BFS.numberOfPaths)
+
+    if(function == "lbfs"):
+        BFS.breath_first_search(InitialStateLong, GoalStateLong)
+
+        lbfsNode = BFS.GoalNode
+        lbfsMoves = []
+
+        while InitialStateLong != lbfsNode.state:
+            if lbfsNode.move == 1:
                 path = 'U'
-            if bfsNode.move == 2:
+            if lbfsNode.move == 2:
                 path = 'D'
-            if bfsNode.move == 3:
+            if lbfsNode.move == 3:
                 path = 'L'
-            if bfsNode.move == 4:
+            if lbfsNode.move == 4:
                 path = 'R'
-            bfsMoves.insert(0, path)
-            bfsNode = bfsNode.parent
+            lbfsMoves.insert(0, path)
+            lbfsNode = lbfsNode.parent
 
         #Print results
-        print("Path: ", bfsMoves)
-        print("Cost: ", len(bfsMoves))
+        print("Path: ", lbfsMoves)
+        print("Cost: ", len(lbfsMoves))
         print("Nodes expanded: ", BFS.numberOfPaths)
 
-    elif(function == "dfs"):
-        (node, nNode) = DFS.depth_first_search(InitialState, GoalState)
+    # elif(function == "dfs"):
+    #     (node, nNode) = DFS.depth_first_search(InitialState, GoalState)
 
-        # dfsNode = DFS.GoalNode
-        dfsMoves = []
+    #     # dfsNode = DFS.GoalNode
+    #     dfsMoves = []
 
-        while InitialState != node.state:
-            if node.move == 1:
-                path = 'U'
-            if node.move == 2:
-                path = 'D'
-            if node.move == 3:
-                path = 'L'
-            if node.move == 4:
-                path = 'R'
-            dfsMoves.insert(0, path)
-            node = node.parent
+    #     while InitialState != node.state:
+    #         if node.move == 1:
+    #             path = 'U'
+    #         if node.move == 2:
+    #             path = 'D'
+    #         if node.move == 3:
+    #             path = 'L'
+    #         if node.move == 4:
+    #             path = 'R'
+    #         dfsMoves.insert(0, path)
+    #         node = node.parent
 
-        #Print results.
-        print("Path: ", dfsMoves)
-        print("Number of moves: ", len(dfsMoves))
-        print("Nodes expanded: ", nNode)
+    #     #Print results.
+    #     print("Path: ", dfsMoves)
+    #     print("Number of moves: ", len(dfsMoves))
+    #     print("Nodes expanded: ", nNode)
 
-    elif(function == "iddfs"):
-        maxDepth = 100
-        (node, nNode) = IDDFS.iterative_deepening_depth_first_search(InitialState, GoalState, maxDepth)
+    # elif(function == "iddfs"):
+    #     maxDepth = 100
+    #     (node, nNode) = IDDFS.iterative_deepening_depth_first_search(InitialState, GoalState, maxDepth)
 
-        iddfsMoves = []
+    #     iddfsMoves = []
 
-        if isinstance(node, PuzzleState):
-            while InitialState != node.state:
-                if node.move == 1:
-                    path = 'U'
-                if node.move == 2:
-                    path = 'D'
-                if node.move == 3:
-                    path = 'L'
-                if node.move == 4:
-                    path = 'R'
-                iddfsMoves.insert(0, path)
-                node = node.parent
+    #     if isinstance(node, PuzzleState):
+    #         while InitialState != node.state:
+    #             if node.move == 1:
+    #                 path = 'U'
+    #             if node.move == 2:
+    #                 path = 'D'
+    #             if node.move == 3:
+    #                 path = 'L'
+    #             if node.move == 4:
+    #                 path = 'R'
+    #             iddfsMoves.insert(0, path)
+    #             node = node.parent
 
-            #Print results.
-            print("Path: ", iddfsMoves)
-            print("Number of moves: ", len(iddfsMoves))
-            print("Nodes expanded: ", nNode)
+    #         #Print results.
+    #         print("Path: ", iddfsMoves)
+    #         print("Number of moves: ", len(iddfsMoves))
+    #         print("Nodes expanded: ", nNode)
 
-        else:
-            print(node)
+    #     else:
+    #         print(node)
 
 if __name__ == '__main__':
     main()
