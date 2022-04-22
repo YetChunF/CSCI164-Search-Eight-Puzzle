7a668c5c5c742806b19a131b81c438d17defc1b8
Juan Mendoza <Juanmendoza97@mail.fresnostate.edu>
2022-04-21 17:50:16 -0700

    added timing to write-files

--- write-files.py
+++ write-files.py
@@ -1,4 +1,5 @@
 import astar
+import time
 
 to_solve = ['865102473', '632754180', '140386527', '142607358']
 
@@ -8,10 +9,12 @@ solutions = []
 for p in to_solve:
     print(f"Solving {p}")
     goal_state = "".join(sorted([c for c in p]))
+    current_time = time.time()
     sol, _ = astar.a_star(p, goal_state)
+    time_elapsed = time.time() - current_time
     print(f"Solution: {sol}")
 
-    f.write(f"Puzzle: {p} Solution: ")
+    f.write(f"Puzzle: {p} Time: {round(time_elapsed, 2)}s Solution: ")
     for state in sol:
         f.write(f"{state}")
     f.write("\n")
